9f73ce5ac8109093eb7296fbb748a59cfae5995c
coderquill <coderquill@gmail.com>
2020-07-19 17:35:51 +0530

    load next group

--- ghunt/src/feed.js
+++ ghunt/src/feed.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Flex, SimpleGrid, Button } from "@chakra-ui/core";
 import { PageHeader } from './components/page-header';
 import { GroupTitle } from './components/group-title';
@@ -15,15 +15,15 @@ function transformFilters({ language, startDate, endDate }) {
     transformedFilters.q = languageQuery + dateQuery;
     transformedFilters.sort = "stars";
     transformedFilters.order = 'desc';
-    
+
     return transformedFilters;
 }
 
 
 export function Feed() {
 
-    const { loading, error, get } = useFetch('https://api.github.com',);
-    
+    const { loading, error, get } = useFetch('https://api.github.com');
+
     const [viewType, setViewType] = useState('grid');
     const [dateJump, setDateJump] = useState('day');
     const [language, setLanguage] = useState();
@@ -36,11 +36,11 @@ export function Feed() {
     useEffect(() => {
         const endDate = moment().subtract(1, 'day').format();
         const startDate = moment(endDate).subtract(1, dateJump).format();
-        
+
         setEndDate(endDate);
         setStartDate(startDate);
 
-        
+
     }, [dateJump, language]);
 
     useEffect(() => {
@@ -56,7 +56,7 @@ export function Feed() {
         get(`/search/repositories?${filtersQuery}`).then((res) => {
             //reposirories has an object : {startDate, endDate, items:[](array of objects)}
             //console.log("res.item"+JSON.stringify(res.data.items));
-            setRepositories([...repositories,{ startDate,endDate, items: res.data.items }]);
+            setRepositories([...repositories, { startDate, endDate, items: res.data.items }]);
             //console.log("repos"+JSON.stringify(repositories));
         });
     }, [startDate]);
@@ -64,8 +64,11 @@ export function Feed() {
     return (
         <Box maxWidth='1200px' mx='auto'>
             <PageHeader />
-            <Flex alignItems='center' justifyContent='space-between' mb = '25px'>
-                <GroupTitle />
+            <Flex alignItems='center' justifyContent='space-between' mb='25px'>
+                <GroupTitle
+                    startDate={startDate}
+                    endDate={endDate}
+                />
                 <Filters
                     viewType={viewType}
                     onViewChange={setViewType}
@@ -76,30 +79,37 @@ export function Feed() {
                 />
             </Flex>
 
-            {repositories.map((repoGroup, counter) => {
+            {repositories.map((repoGroup, counter: number) => {
                 const groupTitle = counter > 0 && (
-                    <GroupTitle
-                        startDate={repoGroup.startDate}
-                        endDate={repoGroup.endDate}
-                    />
+                    <Flex align="center" justify="center">
+                        <GroupTitle
+                            startDate={repoGroup.startDate}
+                            endDate={repoGroup.endDate}
+                        />
+                    </Flex>
+
+
                 )
                 return (
                     <Box>
                         {groupTitle}
                         <SimpleGrid columns={viewType === 'list' ? 1 : 3} spacing='15px'>
-                           {repoGroup.items.map(repo => 
-                            //console.log("each repo"+ JSON.stringify(repo));
-                            <Repo isListView = {viewType==='list'?true:false} repo = {repo} />
-                           )}
+                            {repoGroup.items.map(repo =>
+                                //console.log("each repo"+ JSON.stringify(repo));
+                                <Repo isListView={viewType === 'list' ? true : false} repo={repo} />
+                            )}
                         </SimpleGrid>
                     </Box>
 
                 );
             })}
 
-            
-            <Flex alignItems='center' justifyContent='center' mt='20px' mb = '20px'>
-                <Button variantColor='blue'>Load next group</Button>
+
+            <Flex alignItems='center' justifyContent='center' mt='20px' mb='20px'>
+                <Button isLoading={loading} onClick={() => {
+                    setEndDate(startDate);
+                    setStartDate(moment(startDate).subtract(1, dateJump).format())
+                }} variantColor='blue'>Load next group</Button>
             </Flex>
         </Box>
     );
