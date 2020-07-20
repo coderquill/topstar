0f64a912ade9b7af9a5a569a1d2dd215cfea0402
coderquill <coderquill@gmail.com>
2020-07-20 12:23:04 +0530

    source link fixed

--- topstar/src/components/page-header.js
+++ topstar/src/components/page-header.js
@@ -9,7 +9,7 @@ export function PageHeader() {
         <Flex justifyContent='space-between' alignItems='center' pt='15px'>
             <Brand />
             <Stack isInline >
-                <Button as="a" href="https://github.com/coderquill/githunt/tree/master/ghunt" target="_blank" leftIcon={FaGithub}>View Source</Button>
+                <Button as="a" href="https://github.com/coderquill/topstar" target="_blank" leftIcon={FaGithub}>View Source</Button>
                 <Button as="a" href="https://linkedin.com/in/rupali-kavale" target="_blank" leftIcon={FaLinkedin} variantColor='blue'>LinkedIn</Button>  
                 <Button as="a" href="https://coderquill.github.io/Rupali-Kavale/" target="_blank" leftIcon={RiStarSmileLine} variantColor='red'>About Me</Button>           
             </Stack>
